a79dc3e1eb2804e16b6b8943520ed48679ca7c80
Shannon Young <shannon.young@sagebase.org>
2021-12-13 15:14:54 -0800

    Add task metadata to documented encodings

--- generator/Package.swift
+++ generator/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "generator",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v11),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -17,9 +17,10 @@ let package = Package(
                  from: "4.1.0"),
     ],
     targets: [
+        
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .executableTarget(
             name: "generator",
             dependencies: [
                 .product(name: "Research", package: "SageResearch")
